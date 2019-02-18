a4ded7d2d3bbd7c4817b8121252e0475927370c6
Mohamed Fawzy <mfawzy22@gmail.com>
2019-02-18 15:07:45 +0200

    init server

---
+++ run_keras_server.py
@@ -0,0 +1,6 @@
+from keras.applications import ResNet50
+from keras.preprocessing.image import img_to_array
+from keras.applications import imagenet_utils
+from PIL import Image
+import numpy as np
+
